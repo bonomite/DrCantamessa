ac471e34c8448679dc435230fa11947f6e8ddd43
Thomas Bono <thomasbono.com@gmail.com>
2022-09-10 17:04:22 -0400

    initial setup started

---
+++ nuxt.config.ts
@@ -0,0 +1,6 @@
+import { defineNuxtConfig } from 'nuxt'
+
+// https://v3.nuxtjs.org/api/configuration/nuxt.config
+export default defineNuxtConfig({
+    modules: [['@storyblok/nuxt', { accessToken: 'uGxQ5PnlTEwXpg563G725wtt' }]],
+})
---
+++ pages/index.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { ref, computed, onMounted } from "vue";
+
+const props = defineProps({
+//   propVar: {
+//     type: Boolean,
+//     default: false,
+//   },
+});
+
+//const emit = defineEmits(["change", "click"]);
+
+// lifecycle hooks
+onMounted(() => {
+
+});
+</script>
+
+<template>
+  <div>  
+    <div class="comp-name">
+        Index page
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+.comp-name{
+
+}
+</style>
