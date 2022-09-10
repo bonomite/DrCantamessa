f84a65a3a9a4ee41b64588a3cc93867b812f3d05
Thomas Bono <thomasbono.com@gmail.com>
2022-09-10 18:26:29 -0400

    theme and other modules installed

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,40 @@ import { defineNuxtConfig } from 'nuxt'
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
     modules: [['@storyblok/nuxt', { accessToken: 'uGxQ5PnlTEwXpg563G725wtt' }]],
+    build: {
+        transpile: [
+            'primevue'
+        ]
+    },
+    css: [
+        'primevue-designer/src/assets/themes/vela/vela-purple-dr-cantamessa/vela-purpa-purple-dr-cantamessa.min.css',
+        'primevue/resources/primevue.min.css',
+        'primeicons/primeicons.css',
+
+    ],
+    vite: {
+        css: {
+            preprocessorOptions: {
+                scss: {
+                    // besure to mirror theses imports in the vitest.config.ts
+                    additionalData: `@import url('https://fonts.googleapis.com/css2?family=Baskervville:ital@0;1&family=Open+Sans:wght@400;600&display=swap'); @import "~/assets/scss/global.scss";`,
+                },
+            },
+            postcss: {
+                plugins: [
+                    {
+                        postcssPlugin: 'internal:charset-removal',
+                        AtRule: {
+                            charset: (atRule) => {
+                                if (atRule.name === 'charset') {
+                                    atRule.remove();
+                                }
+                            }
+                        }
+                    }
+                ]
+            }
+        },
+    },
+    components: true,
 })
