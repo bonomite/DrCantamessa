42ddbe53d44059422d07baa2694ea33bfb777c11
Thomas Bono <thomasbono.com@gmail.com>
2022-09-10 21:17:54 -0400

    new storyblok token and axios installed

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-    modules: [['@storyblok/nuxt', { accessToken: 'uGxQ5PnlTEwXpg563G725wtt' }]],
+    modules: [['@storyblok/nuxt', { accessToken: 'D0hiHQaJSnDO3PPrWU818Att' }]],
     build: {
         transpile: [
             'primevue'
