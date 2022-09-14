5980c39d65bac766627196bab46dd5d26eabab92
Thomas Bono <thomasbono.com@gmail.com>
2022-09-14 10:25:53 -0400

    trying to set up articles in surgery

---
+++ pages/[surgerySlug]/[articleSlug].vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { onMounted, onUnmounted, computed } from 'vue'
+
+const config = useRuntimeConfig()
+const route = useRoute()
+
+const article = {}
+
+onMounted(() => {})
+
+onUnmounted(() => {})
+</script>
+
+<template>
+  <div>
+    <Head>
+      <Link rel="canonical" v-if="article" :href="article.url" />
+    </Head>
+    <section v-if="article">
+      <div class="content">Surgery Here</div>
+    </section>
+  </div>
+</template>
+
+<style lang="scss">
+.page.surgery {
+}
+</style>
---
+++ pages/[surgerySlug]/index.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { ref, computed, onMounted } from 'vue'
+
+const props = defineProps({
+  //   propVar: {
+  //     type: Boolean,
+  //     default: false,
+  //   },
+})
+
+//const emit = defineEmits(["change", "click"]);
+
+// lifecycle hooks
+onMounted(() => {})
+</script>
+
+<template>
+  <div>
+    <div class="surgery-index">Surgery Index Page</div>
+  </div>
+</template>
+
+<style lang="scss">
+.surgery-index {
+}
+</style>
