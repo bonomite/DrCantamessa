11254b5785c51cdf67eae9a8ccc52ef50b436a1d
Thomas Bono <thomasbono.com@gmail.com>
2022-09-26 09:09:41 -0400

    margins

--- storyblok/AssetImage.vue
+++ storyblok/AssetImage.vue
@@ -2,7 +2,7 @@
 defineProps({ blok: Object })
 </script>
 <template>
-  <div v-if="blok" v-editable="blok" class="image-blok">
+  <div v-if="blok" v-editable="blok" class="image-blok mb-3">
     <sb-image :src="blok.image" />
   </div>
 </template>
--- storyblok/Embed.vue
+++ storyblok/Embed.vue
@@ -2,7 +2,7 @@
 defineProps({ blok: Object })
 </script>
 <template>
-  <div v-if="blok" v-editable="blok" class="embed-blok">
+  <div v-if="blok" v-editable="blok" class="embed-blok mb-3">
     <EmbedComp :code="blok.embedcode" />
   </div>
 </template>
