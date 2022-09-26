21a37cca36add1ce7ccff818f25335568858beb9
Thomas Bono <thomasbono.com@gmail.com>
2022-09-26 09:04:45 -0400

    added asset-image

---
+++ storyblok/AssetImage.vue
@@ -0,0 +1,8 @@
+<script setup>
+defineProps({ blok: Object })
+</script>
+<template>
+  <div v-if="blok" v-editable="blok" class="image-blok">
+    <sb-image :src="blok.image" />
+  </div>
+</template>
